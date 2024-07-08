6b775b96afa7afe4aa6229832413cb54a48a6106
Indu <indulahari6@gmail.com>
2024-07-08 23:43:54 +0530

    Create Lined PDF

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pandas as pd
 df = pd.read_csv("topics.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False, margin=0)            #so pages won't break
+
+#so pages won't break automatically
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -12,6 +14,11 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.set_text_color(100, 100, 100)
+
+    #For lined PDF
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
+    # Two Lines on top
     pdf.line(10, 21, 200, 21)
 
     #Set the Footer
@@ -20,6 +27,7 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
     pdf.set_text_color(180, 180, 180)
 
+    #nested for-loop
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         # Set the Footer
@@ -27,5 +35,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=12)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
         pdf.set_text_color(180, 180, 180)
+
+        # For lined PDF
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
