e3f870aa96da1f5addf805c6114e5fca1a8e3e80
Indu <indulahari6@gmail.com>
2024-07-08 22:59:11 +0530

    Create multiple pdf pages from csv using for-loop

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv("topics.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello there", align="L", ln=1, border=1)
-
-pdf.set_font(family="Times", style="B", size=9)
-pdf.cell(w=0, h=12, txt="How are you?", align="L", ln=1, border=1)
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.set_text_color(100, 100, 100)
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
