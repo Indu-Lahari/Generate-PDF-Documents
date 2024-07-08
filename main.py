f3e997b58eef7c87b346805bc76c3108eed9a6b3
Indu <indulahari6@gmail.com>
2024-07-08 22:42:24 +0530

    Add pdf font and cells

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 pdf.add_page()
 
+pdf.set_font(family="Times", style="B", size=12)
+pdf.cell(w=0, h=12, txt="Hello there", align="L", ln=1, border=1)
+
+pdf.set_font(family="Times", style="B", size=9)
+pdf.cell(w=0, h=12, txt="How are you?", align="L", ln=1, border=1)
+
 pdf.output("output.pdf")
